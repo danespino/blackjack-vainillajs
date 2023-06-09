df91b70243e5796a06b91c91118c46d4b685f6cd
Dan Espino <darkness91@gmail.com>
2023-06-09 16:50:51 -0600

    Changed deck creation and styling

--- assets/js/blackjack.js
+++ assets/js/blackjack.js
@@ -1,66 +1,34 @@
 let playerCounter = 0, playerDrawCards = 0, pcCounter = 0, pcDrawCards = 0, lastCard = '';
 let selectedCards = [], playerCardsHistory = [], pcCardsHistory = [];
 
-const cards = [
-    'AH','2H','3H','4H','5H','6H','7H','8H','9H','10H','JH','QH','KH','AD','2D','3D','4D','5D','6D','7D','8D','9D','10D','JD','QD','KD','AC','2C','3C','4C','5C','6C','7C','8C','9C','10C','JC','QC','KC','AS','2S','3S','4S','5S','6S','7S','8S','9S','10S','JS','QS','KS'
-];
+let cards = [], cardsVals=[];
+const types = ['C', 'H', 'S', 'D'];
+const majors = ['J', 'Q', 'K', 'A'];
 
-const cardsVals = [ 
-                {"id":'AH',   "value":11},
-                {"id":'2H',   "value": 2},
-                {"id":'3H',   "value": 3},
-                {"id":'4H',   "value": 4},
-                {"id":'5H',   "value": 5},
-                {"id":'6H',   "value": 6},
-                {"id":'7H',   "value": 7},
-                {"id":'8H',   "value": 8},
-                {"id":'9H',   "value": 9},
-                {"id":'10H',  "value":10},
-                {"id":'JH',   "value":10},
-                {"id":'QH',   "value":10},
-                {"id":'KH',   "value":10},
-                {"id":'AD',   "value":11},
-                {"id":'2D',   "value": 2},
-                {"id":'3D',   "value": 3},
-                {"id":'4D',   "value": 4},
-                {"id":'5D',   "value": 5},
-                {"id":'6D',   "value": 6},
-                {"id":'7D',   "value": 7},
-                {"id":'8D',   "value": 8},
-                {"id":'9D',   "value": 9},
-                {"id":'10D',  "value":10},
-                {"id":'JD',   "value":10},
-                {"id":'QD',   "value":10},
-                {"id":'KD',   "value":10},
-                {"id":'AC',   "value":11},
-                {"id":'2C',   "value": 2},
-                {"id":'3C',   "value": 3},
-                {"id":'4C',   "value": 4},
-                {"id":'5C',   "value": 5},
-                {"id":'6C',   "value": 6},
-                {"id":'7C',   "value": 7},
-                {"id":'8C',   "value": 8},
-                {"id":'9C',   "value": 9},
-                {"id":'10C',  "value":10},
-                {"id":'JC',   "value":10},
-                {"id":'QC',   "value":10},
-                {"id":'KC',   "value":10}, 
-                {"id":'AS',   "value":11},
-                {"id":'2S',   "value": 2},
-                {"id":'3S',   "value": 3},
-                {"id":'4S',   "value": 4},
-                {"id":'5S',   "value": 5},
-                {"id":'6S',   "value": 6},
-                {"id":'7S',   "value": 7},
-                {"id":'8S',   "value": 8},
-                {"id":'9S',   "value": 9},
-                {"id":'10S',  "value":10},
-                {"id":'JS',   "value":10},
-                {"id":'QS',   "value":10},
-                {"id":'KS',   "value":10}, 
-];
+const createDeck = () =>{
+    for(let i=2; i<=10; i++){
+        for (const type of types) {
+            cards.push(i+type);
+            cardsVals.push({"id":i+type,  "value":i});
+        }
 
-Object.freeze(cards);
+        if(i==10){
+            for(const major of majors){
+                for(const type of types){
+                    cards.push(major+type);
+                    if(major!="A"){
+                        cardsVals.push({"id":major+type, "value":i});
+                    } else {
+                        cardsVals.push({"id":major+type, "value":11});
+                    }
+                }
+            }
+        }
+    }
+}
+
+createDeck();
+Object.freeze(cards, cardsVals);
 
 const initGame = () => {
     clearHand();
@@ -94,7 +62,7 @@ const drawCard = (player) => {
 
             }
             let liElement = document.createElement('li');
-            liElement.innerHTML=`<img src="js/blackjack/assets/img/${lastCard}.png" alt="${lastCard}" />`;
+            liElement.innerHTML=`<img src="assets/img/${lastCard}.png" alt="${lastCard}" />`;
             drawnCardsUI.appendChild(liElement);
 
             if(player=='player1'){
