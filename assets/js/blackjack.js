354f2f15badec2b50e5c05f53080c56f56da1289
danespino <darkness91@gmail.com>
2024-06-25 21:19:19 -0600

    Adds some functions to handle states. drawing cards, etc. Merge to main

--- assets/js/blackjack.js
+++ assets/js/blackjack.js
@@ -1,5 +1,6 @@
 let playerCounter = 0, playerDrawCards = 0, pcCounter = 0, pcDrawCards = 0, lastCard = '', playerName = "Jugador 1";
 let selectedCards = [], playerCardsHistory = [], pcCardsHistory = [];
+var turn = 1;        // 1 - Player, 2 - Computer
 
 let cards = [], cardsVals=[];
 const types = ['C', 'H', 'S', 'D'];
@@ -37,7 +38,7 @@ const initGame = () => {
     clearHand();
     drawHand('player1');
     drawHand('computer');
-    evalHands();
+    handleStats('newGame');
 }
 
 const randomCard = () => (Math.floor(Math.random() * cards.length));
@@ -65,8 +66,11 @@ const drawCard = (player) => {
             // We add the card to the history array of the player
             if(player=='player1'){
                 playerCardsHistory.push(lastCard);
+                document.getElementById('playerCounter').innerHTML = playerCounter;
+                if(playerCounter >= 15) document.getElementById('endTurnBtn').disabled = false;
             } else {
                 pcCardsHistory.push(lastCard);
+                document.getElementById('pcCounter').innerHTML = pcCounter;
             }
             // We draw the card in the UI
             let liElement = document.createElement('li');
@@ -76,7 +80,7 @@ const drawCard = (player) => {
     }
 }
 
-const evalIfAce = (player, cardValue) =>{
+const evalIfAce = (player, cardValue) => {
     let isAce = lastCard.includes('A') ? true : false;
     if(isAce){
         if(player=='player1'){
@@ -99,7 +103,7 @@ const evalIfAce = (player, cardValue) =>{
     }
 }
 
-const clearHand = () =>{
+const clearHand = () => {
     playerCounter = 0;
     playerDrawCards = 0;
     pcCounter = 0;
@@ -111,25 +115,36 @@ const clearHand = () =>{
 }
 
 const drawHand = (player) => {
-    const cardsPerHand = 3;
+    const cardsPerHand = 2;
+
     for (let index = 1; index <= cardsPerHand; index++) {
         drawCard(player);
     }
-    if(player=='player1'){
+
+    if(player=='player1') {
         document.getElementById('playerCounter').innerHTML = playerCounter;
     } else{
         document.getElementById('pcCounter').innerHTML = pcCounter;
     }
 }
 
-const evalHands = (firstHand = true) => {
+const evalHands = () => {
     if(playerCounter > 21 && pcCounter <= 21){
         drawResult('loss');
     } else if(playerCounter <= 21 && pcCounter > 21){
         drawResult('won');
     } else if(playerCounter > 21 && pcCounter > 21){
         drawResult('tie');
+    } else if(playerCounter <= 21 && pcCounter <= 21) {
+        if(playerCounter > pcCounter){
+            drawResult('won');
+        } else if(playerCounter < pcCounter){
+            drawResult('loss');
+        } else {
+            drawResult('tie');      // Less likely to happen, but covered if in any case
+        }
     }
+    handleStats('endGame');
 }
 
 const drawResult = (result) => {
@@ -167,4 +182,49 @@ const drawResult = (result) => {
         document.getElementById('tableScore').appendChild(img);
     }
     document.getElementById('tableScore').appendChild(status);
+}
+
+const handleStats = (gameStat) => {
+    switch(gameStat) {
+        case 'newGame':
+            turn = 1;
+            document.getElementById('drawCardBtn').disabled = false;
+            document.getElementById('newGameBtn').disabled = true;
+            document.getElementById('newGameBtn').style.display = "none";
+            if(playerCounter >= 15) document.getElementById('endTurnBtn').disabled = false;
+        break;
+        case 'endGame':
+            document.getElementById('newGameBtn').style.display = "inline";
+            document.getElementById('newGameBtn').disabled = false;
+            document.getElementById('drawCardBtn').disabled = true;
+            document.getElementById('endTurnBtn').disabled = true;
+        break;
+    }
+}
+
+const drawCardUser = () => {
+    if(turn == 1){
+        drawCard('player1');
+        if(playerCounter == 21) evalHands();
+    } else {
+        alert('This should never be reached! Violation Attempt!');
+    }
+}
+
+const stopTurn = () => {
+    if(turn == 1){
+        turn = 2;
+        pcTurn();
+    } else {
+        alert('This should never be reached! Violation Attempt!');
+    }
+}
+
+const pcTurn = () => {
+    if(turn == 2){
+        while(pcCounter<15){
+            drawCard('computer');
+        }
+        evalHands();
+    } 
 }
